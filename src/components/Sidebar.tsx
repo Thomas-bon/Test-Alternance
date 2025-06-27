76835240797b759fb489ba5baa54a5d076559880
Thomas-bon <pro.bonanseathomas@gmail.com>
2025-06-28 00:23:59 +0200

    feat: enhance Sidebar and Dashboard components with new icons and styling adjustments

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,25 +1,103 @@
 import React from 'react'
 
+
 const SideBar = () => {
-  
+
     return (
-    <div className="h-full flex flex-col justify-between items-center py-8 ">
-        
-        <div className="w-10 h-10 bg-red-600 mt-8 cursor-pointer"></div>
+        <div className="h-full flex flex-col justify-between items-center py-8 ">
 
-      {/* Carrés bleus au centre */}
-      <div className="flex flex-col items-center gap-15">
-        <div className="w-10 h-10 bg-blue-500 cursor-pointer"></div>
-        <div className="w-10 h-10 bg-blue-500 cursor-pointer"></div>
-        <div className="w-10 h-10 bg-blue-500 cursor-pointer"></div>
-        <div className="w-10 h-10 bg-blue-500 cursor-pointer"></div>
-      </div>
+            {/* Logo en haut */}
+            <div className="w-9 h-9 bg-red-600 mt-8 cursor-pointer"></div>
 
-      {/* Carré jaune en bas */}
-      <div className="w-10 h-10 bg-yellow-400 mb-8 cursor-pointer"></div>
+            {/* Boutons nav au centre */}
+            <div className="flex flex-col items-center gap-15">
 
-    </div>
-  )
+                <div className="w-9 h-9 flex items-center justify-center cursor-pointer rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="w-full h-full text-gray-300 group-hover:text-yellow-300 transition-colors"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    </svg>
+                </div>
+
+                <div className="w-9 h-9 flex items-center justify-center cursor-pointer rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="w-full h-full text-gray-300 group-hover:text-yellow-300 transition-colors"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+                    </svg>
+
+                </div>
+
+                <div className="w-9 h-9 flex items-center justify-center cursor-pointer rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="w-full h-full text-gray-300 group-hover:text-yellow-300 transition-colors"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
+                    </svg>
+
+
+
+                </div>
+
+                <div className="w-9 h-9 flex items-center justify-center cursor-pointer rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="w-full h-full text-gray-300 group-hover:text-yellow-300 transition-colors"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+
+                </div>
+            </div>
+
+            {/* Boutons setting en bas */}
+            <div className="w-9 h-9 flex items-center mb-8 justify-center cursor-pointer rounded">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-full h-full text-gray-500 group-hover:text-yellow-300 transition-colors"
+                >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default SideBar
--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -21,7 +21,7 @@ const DashboardPage = () => {
                 <div className='flex gap-5'>
                     <div className='h-18 w-60 bg-white rounded-2xl border-2 border-gray-200 drop-shadow-sm flex items-center px-4'>
                         <svg
-                            className="w-6 h-6 text-gray-400 mr-3 flex-shrink-0"
+                            className="w-7 h-7 text-gray-400 mr-3 flex-shrink-0"
                             fill="none"
                             stroke="currentColor"
                             strokeWidth="2"
@@ -35,11 +35,26 @@ const DashboardPage = () => {
                             value={searchTerm}
                             onChange={(e) => setSearchTerm(e.target.value)}
                             onKeyDown={handleKeyEnterDown}
-                            placeholder="Recherche..."
-                            className="bg-transparent flex-grow outline-none text-gray-400 placeholder-gray-400"
+                            placeholder="Recherche"
+                            className="bg-transparent flex-grow outline-none text-gray-400 placeholder-gray-400 placeholder:font-medium"
                         />
                     </div>
-                    <div className='h-18 w-18 bg-[#111725] rounded-2xl'></div>
+                    <div className='h-18 w-18 bg-[#111725] rounded-2xl flex justify-center items-center'>
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            strokeWidth={1.5}
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="w-[50%] h-[50%] text-white group-hover:text-yellow-300 transition-colors"
+                        >
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                        </svg>
+
+
+                    </div>
                 </div>
             </div>
         </div>
